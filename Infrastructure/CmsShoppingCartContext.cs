3cd9f7dbb3fc7b8ded9282a0aee21e5e03145356
Dominik Twaróg <dominik.twarog@cyfrowafoto.com>
2020-01-12 12:07:07 +0100

    Db context for page and initial migration

---
+++ Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext :DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options):base(options)
+        {
+            
+        }
+
+        private DbSet<Page> Pages { get; set; }
+    }
+}
