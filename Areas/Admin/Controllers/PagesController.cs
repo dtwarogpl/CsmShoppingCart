d9c604752931e534222e68f45b89dc9af6c00776
Dominik Twaróg <dominik.twarog@cyfrowafoto.com>
2020-01-12 14:10:21 +0100

    Added page Create and details actions

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -19,7 +19,7 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         {
             _context = context;
         }
-
+        //GET /admin/pages
         public async Task<IActionResult> Index()
         {
             IQueryable<Page> pages = from p in _context.Pages orderby p.Sorting select p;
@@ -28,6 +28,51 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             return View(pagesList);
 
 
+
+        }
+        
+        //GET /admin/pages
+        public async Task<IActionResult> Details(int id)
+        {
+
+            var page = await _context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(page==null) { return NotFound(); }
+
+            return View(page);
+
+
+
+        }
+
+        //GET /admin/pages/create
+        public async Task<IActionResult> Create() => View();
+
+        //POST /admin/pages/create
+        [HttpPost]
+        public async Task<IActionResult> Create(Page page)
+        {
+
+            if(ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower()
+                                .Replace(" ", "-");
+                page.Sorting = 100;
+
+                var slug = await _context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if(slug!=null)
+                {
+                    ModelState.AddModelError("","The title already exists");
+                    return View(page);
+                }
+
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(page);
 
         }
     }
--- Models/Page.cs
+++ Models/Page.cs
@@ -9,9 +9,11 @@ namespace CmsShoppingCart.Models
     public class Page
     {
         public int Id { get; set; }
-        [Required] public string Title { get; set; }
-        [Required] public string Slug { get; set; }
-        [Required] public string Content { get; set; }
+        [Required, MinLength(2,ErrorMessage = "Minimum length is 2")] 
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+        public string Content { get; set; }
         public int Sorting { get; set; }
     }
 }
