65b73a9af5ef899a5d04e269f95a68a976b4e1fa
Dominik Twaróg <dominik.twarog@cyfrowafoto.com>
2020-01-12 11:28:51 +0100

    db

---
+++ Infrastructure/CsmShoppingCartContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CsmShoppingCartContext :DbContext
+    {
+        public CsmShoppingCartContext(DbContextOptions<CsmShoppingCartContext> options):base(options)
+        {
+            
+        }
+    }
+}
