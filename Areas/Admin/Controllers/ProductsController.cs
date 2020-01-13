e9a2bb32691fde5478a188761f46df564ae61330
Dominik Twaróg <dominik.twarog@cyfrowafoto.com>
2020-01-13 20:12:48 +0100

    Products model, list and create post method added

---
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+      
+        private readonly CmsShoppingCartContext _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ProductsController(CmsShoppingCartContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET admin/products/
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.OrderBy(x => x.Id).Include(x=>x.Category).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+
+            ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+            return View();
+        }
+
+
+
+        //POST /admin/products/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower()
+                                .Replace(" ", "-");
+                
+
+                var slug = await _context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists");
+                    return View(product);
+                }
+
+
+                string imageName = "noimage.png";
+                if(product.ImageUpload!=null)
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    imageName = Guid.NewGuid()
+                                    .ToString() +
+                                "_" +
+                                product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadsDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+
+                   
+                }
+                product.Image = imageName;
+
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been created";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+
+        }
+
+    }
+}
---
+++ Infrastructure/FileExtensionAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class FileExtensionAttribute: ValidationAttribute
+    {
+
+        string[] extensions = new[] { "jpg", "png" };
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if(file!=null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+
+              
+
+                bool result = extensions.Any(x => extension.EndsWith(x));
+
+                if(!result)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+
+                
+            }
+            return ValidationResult.Success;
+
+        }
+
+        private string GetErrorMessage()
+        {
+            return $"Allowed extensions are: {string.Join(",", extensions)}";
+        }
+    }
+}
