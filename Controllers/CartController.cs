a4408824565e40a28fcdfa7b7a17f8e2184f8d3a
dtwarogpl <twarog.dominik@gmail.com>
2020-01-20 19:44:59 +0100

    Cart functions added

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -52,5 +52,62 @@ namespace CmsShoppingCart.Controllers
             HttpContext.Session.SetJson("Cart", cart);
             return RedirectToAction("Index");
         }
+
+        // GET /cart/decrease/5
+        public IActionResult Decrease(int id)
+        {
+           
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            CartItem cartItem = cart
+                    .FirstOrDefault(x => x.ProductId == id);
+            if(cartItem.Quantity > 1)
+            {
+                cartItem.Quantity--;
+
+            }
+            else { cart.RemoveAll(x => x.ProductId == id); }
+            if(cart.Count==0)
+            {
+                HttpContext.Session.Remove("Cart");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET /cart/remove/5
+        public IActionResult Remove(int id)
+        {
+
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            CartItem cartItem = cart
+                    .FirstOrDefault(x => x.ProductId == id);
+            cart.RemoveAll(x => x.ProductId == id);
+
+
+            if (cart.Count == 0)
+            {
+                HttpContext.Session.Remove("Cart");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        // GET /cart/clear
+        public IActionResult Clear()
+        {
+            HttpContext.Session.Remove("Cart");
+            return RedirectToAction("Index");
+        }   
     }
 }
