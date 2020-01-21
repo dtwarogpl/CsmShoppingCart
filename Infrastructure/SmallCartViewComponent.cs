4ad12ca6274ea1942cbfffe13e099ea9b32f479c
dtwarogpl <twarog.dominik@gmail.com>
2020-01-21 17:51:38 +0100

    Small cart funcionality added

---
+++ Infrastructure/SmallCartViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class SmallCartViewComponent: ViewComponent
+
+    
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<CartItem> Cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+            SmallCartViewModel smallCartViewModel;
+            if(Cart == null ||
+               Cart.Count == 0)
+            {
+                smallCartViewModel = null;
+            }
+            else
+            {
+                smallCartViewModel = new SmallCartViewModel
+                                     {
+                                             NumberofItems = Cart.Sum(x => x.Quantity), TotalAmount = Cart.Sum(x => x.Quantity * x.Price)
+                                     };
+            }
+
+            return View(smallCartViewModel);
+        }
+    }
+}
