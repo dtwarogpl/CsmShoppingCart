5a0abef1145f3cd319defe133209e4a35a0a3eb8
dtwarogpl <twarog.dominik@gmail.com>
2020-01-23 19:45:28 +0100

    Basic register and login operations

--- Startup.cs
+++ Startup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,53 +30,60 @@ namespace CmsShoppingCart
         {
             services.AddMemoryCache();
             services.AddSession(
-                        //options => TimeSpan.FromDays(2)
-                        );
+
+                    //options => TimeSpan.FromDays(2)
+            );
             services.AddControllersWithViews();
             services.AddDbContext<CmsShoppingCartContext>(
                     options => options.UseSqlServer(Configuration.GetConnectionString("CmsShoppingCartContext")));
+
+            services.AddIdentity<AppUser, IdentityRole>(
+                            options =>
+                            {
+                                options.Password.RequiredLength = 4;
+                                options.Password.RequireNonAlphanumeric = false;
+                                options.Password.RequireLowercase = false;
+                                options.Password.RequireUppercase = false;
+                                options.Password.RequireDigit = false;
+                            })
+                    .AddEntityFrameworkStores<CmsShoppingCartContext>()
+                    .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if(env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
             app.UseSession();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+           
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                        "pages","{slug?}", defaults: new { controller = "Pages", action= "Page"}
-                        );  
-                
-                endpoints.MapControllerRoute(
-                        "products",
-                        "products/{categorySlug}", defaults: new { controller = "Products", action= "ProductsByCategory"}
-                        );
+            app.UseEndpoints(
+                    endpoints =>
+                    {
+                        endpoints.MapControllerRoute("pages", "{slug?}", defaults: new {controller = "Pages", action = "Page"});
 
-                endpoints.MapControllerRoute(
-                        name: "areas",
-                        pattern: "{area:exists}/{controller=Pages}/{action=Index}/{id?}"
-                );
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+                        endpoints.MapControllerRoute(
+                                "products",
+                                "products/{categorySlug}",
+                                defaults: new {controller = "Products", action = "ProductsByCategory"});
+
+                        endpoints.MapControllerRoute(name: "areas", pattern: "{area:exists}/{controller=Pages}/{action=Index}/{id?}");
+                        endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+                    });
         }
     }
 }
